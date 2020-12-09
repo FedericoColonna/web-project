25c160df776a8023636730ceee9a84a83cebd23c
Federico Colonna <federico.colonna89@gmail.com>
2020-12-09 23:20:53 +0100

    add files

--- loginpage.php
+++ loginpage.php
@@ -4,8 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
 <title>Login</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
- integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="node_modules/bootswatch/dist/cyborg/bootstrap.min.css">
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
  crossorigin="anonymous"></script>
@@ -31,8 +30,16 @@ integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZD
                     </div>
                 </div>
                 <form action="/back_end/login.php" method="POST">
-                    <div> <input class="loginfield" type="email" id="email" name="email" placeholder="Email"> </div>
-                    <div> <input class="loginfield" type="password" id="pass" name="pass" placeholder="Password"> </div>
+
+                <div class="form-group has-success">
+                    <label class="form-control-label" for="inputValid">Email</label>
+                    <input id="email" type="email" name="email" class="form-control" id="inputValid">
+                </div>
+                <div class="form-group has-success">
+                    <label class="form-control-label" for="inputValid">Password</label>
+                    <input id="pass" type="password"  name="pass" class="form-control" id="inputValid">
+                </div>
+
                     <div class="row">
                         <div class="col-md-6 col-12">
                             
@@ -40,7 +47,7 @@ integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZD
                     
                     </div>
                     <div class="form-group mt-3">
-                        <button type="submit" name="login" class="btn btn-block btn-lg"><i>Login</i></button>
+                        <button type="submit" name="login" class="btn btn-primary">Login</button>
                     </div>
                 </form>
             </div>
